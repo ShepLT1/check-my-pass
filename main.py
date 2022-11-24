c6f6ead3f47d464349570945a7141d7efa4f8ec0
ShepLT1 <luke.t.shepherd@gmail.com>
2022-11-23 18:40:09 -0800

    added ability to check pwnedpasswords with first 5 characters of hash

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ def request_api_data(query_char):
     if res.status_code != 200:
         raise RuntimeError(f'Error fetching: {res.status_code}, check API & try again')
     return res
+
+def pwned_api_check(password):
+    sha1pass = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5_char, tail = sha1pass[0:5], sha1pass[5:]
+    response = request_api_data(first5_char)
+    return response
